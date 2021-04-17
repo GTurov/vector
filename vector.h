9fb7748a3f5b487ab8e33ff7738c4def91f21347
Gennadiy Turov <mirage216@mail.ru>
2021-04-17 22:01:38 +0400

    correct1

--- vector.h
+++ vector.h
@@ -239,6 +239,7 @@ public:
 
     template <typename... Args>
     iterator Emplace(const_iterator pos, Args&&... args) {
+        CheckIterator(pos);
         uint64_t element_number = pos - data_.GetAddress();
         if (size_ != data_.Capacity() && data_.Capacity() != 0) {
             if (element_number != size_) {
@@ -280,6 +281,7 @@ public:
     }
 
     iterator Erase(const_iterator pos) /*noexcept(std::is_nothrow_move_assignable_v<T>)*/ {
+        CheckIterator(pos);
         uint64_t n = pos - data_.GetAddress();
         std::move(data_ + n + 1, end(), data_ + n);
         data_[size_ - 1].~T();
@@ -296,6 +298,11 @@ public:
     }
 
 private:
+    void CheckIterator(const_iterator pos) {
+        if (pos < data_.GetAddress() || pos > data_+size_) {
+            throw std::out_of_range("wrong iterator");
+        }
+    }
     RawMemory<T> data_;
     size_t size_ = 0;
 };
